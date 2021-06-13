baa0e7c6ffbee75baa8c3be0d9ee085e584cb764
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2021-06-13 13:14:28 -0300

    Segundo cambio Probando como funciona un event handler de react y su rederizado.

--- src/eventHandler.js
+++ src/eventHandler.js
@@ -1,8 +1,8 @@
-const nombre = 'Julián';
+const nombre = 'Agustin';
 
 const elemento = (
   <button
-    onClick={event => alert(`Hiciste click en el botón con texto "${event.target.innerText}"`)}
+    onClick={event => alert(`Hiciste click en el botón con texto "${event.target.innerText}" y el nombre es ${nombre}`)}
   >
     Hacé click
   </button>
